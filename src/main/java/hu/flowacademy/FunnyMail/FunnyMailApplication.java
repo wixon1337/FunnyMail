b241533438356bd8b5b9c63dc1389cc58250731c
Vilmos Pásztor <pvilmos93@gmail.com>
2019-09-10 09:25:48 +0200

    project generated

---
+++ src/main/java/hu/flowacademy/FunnyMail/FunnyMailApplication.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.FunnyMail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunnyMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunnyMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/flowacademy/FunnyMail/FunnyMailApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.flowacademy.FunnyMail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FunnyMailApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
